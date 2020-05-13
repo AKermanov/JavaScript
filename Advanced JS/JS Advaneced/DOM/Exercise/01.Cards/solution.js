288f6dba3cb6fd57ce9827ca6c6b4a30eeaef0b7
Aleksandar Kermanov <58025467+fav0rableCode@users.noreply.github.com>
2020-05-13 13:50:34 +0300

    Add files via upload

---
+++ Advanced JS/JS Advaneced/DOM/Exercise/01.Cards/solution.js
@@ -0,0 +1,57 @@
+function solve() {
+  Array.from(document.getElementsByTagName('img')).forEach((img) => {
+    img.addEventListener('click', clickEvent);
+  });
+}
+
+function clickEvent(e) {
+  let card = e.target;
+  card.src = './images/whiteCard.jpg';
+  let parent = card.parentNode;
+  card.removeEventListener('click', clickEvent);
+  let spans = document.getElementById('result').children;
+
+  let leftSpan = spans[0];
+  let rightSpan = spans[2];
+  if (parent.id === 'player1Div') {
+    leftSpan.textContent = card.name;
+  } else if (parent.id === 'player2Div') {
+    rightSpan.textContent = card.name;
+  }
+
+  if (spans[0].textContent && spans[2].textContent) {
+    let winner;
+    let looser;
+    if (+spans[0].textContent > +spans[2].textContent) {
+      //stting the borderrightSpan
+      winner = document.querySelector(
+        `#player1Div img[name="${leftSpan.textContent}"]`
+      );
+      looser = document.querySelector(
+        `#player2Div img[name="${rightSpan.textContent}"]`
+      );
+    } else {
+      winner = document.querySelector(
+        `#player2Div img[name="${rightSpan.textContent}"]`
+      );
+      looser = document.querySelector(
+        `#player1Div img[name="${leftSpan.textContent}"]`
+      );
+    }
+    winner.style.border = '2px solid green';
+    looser.style.border = '2px solid darkred';
+
+    document.getElementById(
+      'history'
+    ).textContent += `[${leftSpan.textContent} vs ${rightSpan.textContent}] `;
+
+    //за да мине в judge
+    // leftSpan.textContent = "";
+    // rightSpan.textContent = "";
+
+    setTimeout(function () {
+      leftSpan.textContent = '';
+      rightSpan.textContent = '';
+    }, 2000);
+  }
+}
