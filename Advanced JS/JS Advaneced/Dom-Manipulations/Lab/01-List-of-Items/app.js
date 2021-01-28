51c8b5c9cfd139fc18e886997f4dc3dbc0bba989
Aleksandar Kermanov <58025467+AKermanov@users.noreply.github.com>
2021-01-28 16:05:41 +0200

    Create app.js

---
+++ Advanced JS/JS Advaneced/Dom-Manipulations/Lab/01-List-of-Items/app.js
@@ -0,0 +1,8 @@
+function addItem() {
+    const liElement = document.createElement('li');
+
+    liElement.textContent = document.getElementById('newItemText').value;
+    document.getElementById('items').appendChild(liElement);
+
+    document.getElementById('newItemText').value = '';
+}
