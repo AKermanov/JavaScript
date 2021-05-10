107f8af1b06497cc9ddb54c9cbd615e72f970860
Aleksandar Kermanov <58025467+AKermanov@users.noreply.github.com>
2021-05-10 12:16:21 +0300

    Create 04-Sum-of-Numbers.js

---
+++ Advanced JS/JS Advaneced/Unit Testing and Error Handling/Lab/04-Sum-of-Numbers.js
@@ -0,0 +1,13 @@
+describe('Sum numbers', ()=>{
+    it('sums single number number', ()=>{
+        expect(sum([1])).to.equal(1);
+    });
+
+    it('sums multiple numbers', ()=>{
+        expect(sum([1, 1])).to.equal(2);
+    });
+
+    it('sums different numbers', ()=>{
+        expect(sum([1, 2, 3])).to.equal(6);
+    })
+});
