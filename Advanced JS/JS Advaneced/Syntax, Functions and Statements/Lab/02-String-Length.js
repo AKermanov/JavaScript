6d21deedd6ee4cdcf5dec59586cfe9da6e4d08dd
Aleksandar Kermanov <58025467+AKermanov@users.noreply.github.com>
2021-05-11 21:30:43 -0700

    Create 02-String-Length.js

---
+++ Advanced JS/JS Advaneced/Syntax, Functions and Statements/Lab/02-String-Length.js
@@ -0,0 +1,10 @@
+function solve(a, b, c) {
+    let sum = 0;
+    let elements = [a, b, c];
+    elements.forEach(e =>
+        sum += e.length
+    );
+    let avgL = sum / 3;
+    console.log(sum);
+    console.log(Math.floor(avgL));
+}
